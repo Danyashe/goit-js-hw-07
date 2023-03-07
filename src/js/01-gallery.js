74850d3beb23eda80f3caf77799ac12048185a42
Danyashe <27shdaniil@gmail.com>
2023-03-07 22:09:26 +0200

    hw-js-7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryItem = galleryItems.map((image) => 
+`<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>`
+).join("");
+
+galleryRef.insertAdjacentHTML('beforeend', galleryItem);
+
+galleryRef.addEventListener('click', getBigImage);
+
+function getBigImage(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const bigImage = e.target.dataset.source
+    console.log(bigImage)
+const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${bigImage}">
+	`)
+  instance.show()
+
+    document.addEventListener("keydown",(e) => {
+          if (e.code !== "Escape") {
+            return;
+          }
+          instance.close();
+        },
+        { once: true }
+      );
+}
